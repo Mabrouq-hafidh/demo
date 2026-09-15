0c25cc9847d2281a1eef7a9c30d2ee57837a05a5
Hafidh <hafidh@MACs-MacBook-Pro.local>
2026-09-15 17:01:33 +0300

    first commit

---
+++ demo2.py
@@ -0,0 +1,11 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+
+print("----------------------")
+print("STUDENT INFORMATION")
+print("----------------------")
+
+print("Name:", name)
+print("Age:", age)
+print("Course:", course)
